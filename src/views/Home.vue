d18ebf4f8172e9ab2578634f555d1e9e6afa3d85
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-15 11:10:33 +0100

    home update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,47 @@
 <template lang="pug">
 #Home
-  h1 hello world
+  header
+    h1 Welcome to chatbot-generator
+    p Create all your questions and answer for your chatbot.
+  .content
+    Generator(v-if="open")
+    b-button(variant="outline-primary", @click="openGenerator") + Create a question
 </template>
 
 <script>
+import Generator from '../components/Generator.vue'
+
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    Generator
+  },
+  methods: {
+    openGenerator() {
+      this.open = true
+    }
+  },
+  data() {
+    return {
+      open: false,
+      model: {
+        data: {
+
+        }
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 #Home {
+  text-align: left;
+
+  header {
+    margin: 20px 0;
+  }
+
   img {
     max-width: 400px;
   }
