042c3be31de272d0f6ca1a07a6b894ca3b46d295
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-17 14:05:11 +0100

    ErrorMessage.vue

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  #ErrorMessage
+    p.text-danger {{ title }}
+</template>
+
+<script>
+export default {
+  name: 'ErrorMessage',
+  props: {
+    title: String
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#ErrorMessage {
+
+}
+</style>
