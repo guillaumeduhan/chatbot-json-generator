90209b2567d16e08d1ce6cd836691520c9cf1a69
Guillaume Duhan <myhappyagency@gmail.com>
2020-02-17 14:05:18 +0100

    Generator.vue

--- src/components/Generator.vue
+++ src/components/Generator.vue
@@ -2,6 +2,9 @@
   #Generator
     h2 Create a new question
     .line.title
+      h5 Question number:
+      b-input(placeholder="001", v-model="question.number")
+    .line.title
       h5 Question title:
       b-input(placeholder="What is your favorite desert?", v-model="question.title")
     .line.range
@@ -11,20 +14,44 @@
         .delay {{ question.delay }} secs.
     .line.next
       h5(for="range-1") Does my question has answers?
-      b-form-radio(name="next-radio", v-model="question.next", value="false") Yes, my question has answers
-      b-form-radio(name="next-radio", v-model="question.next", value="true") No, my question has no answer, switch to new one
+      b-form-radio(name="next-radio", v-model="question.next", value="true") Yes, my question has answers
+      b-form-radio(name="next-radio", v-model="question.next", value="false") No, my question has no answer, go to next
+    .line.answers--buttons(v-if="question.next === 'true'")
+      h5 Add your answers
+      AnswerGenerator(@answerSend="pushAnswerToAnswers")
 </template>
 
 <script>
+import AnswerGenerator from './AnswerGenerator.vue'
+
 export default {
   name: 'Generator',
+  components: {
+    AnswerGenerator
+  },
   data() {
     return {
       question: {
+        number: undefined,
         title: undefined,
-        delay: 0,
-        answers: {},
-        next: false
+        delay: 2,
+        answers: undefined,
+        next: 'true'
+      }
+    }
+  },
+  methods: {
+    pushAnswerToAnswers(answer) {
+      let size;
+      if (this.question.answers) {
+        size = Object.keys(this.question.answers).length;
+        size = size + 1
+        this.question.answers[size] = answer
+      } else {
+        size = 1
+        this.question.answers = {
+          [size]: answer
+        }
       }
     }
   }
@@ -38,6 +65,7 @@ export default {
   border-radius: 5px;
   border: 1px solid #ddd;
   margin-bottom: 15px;
+  max-width: 600px;
 
   .line {
     margin: 25px 0;
